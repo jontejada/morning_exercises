bb8b549d7135c2679d1938aee7e677b3ec49fcfa
Jon Tejada <tejjon@gmail.com>
2015-12-04 13:27:25 -0800

    parens now also deals with [] and {}

--- parens.js
+++ parens.js
@@ -10,8 +10,10 @@
 */
 
 function parensChecker(input){
-	while (input.indexOf('()') >= 0 ) {
+	while (input.indexOf('()') >= 0 || input.indexOf('[]') >= 0 || input.indexOf('{}') >= 0) {
 		input = input.replace('\(\)','');
+		input = input.replace('\{\}','');
+		input = input.replace('\[\]','');
 	}
 	if (input === '') {
 		return true;
@@ -19,13 +21,22 @@ function parensChecker(input){
 	return false;
 }
 
-
 function test(parensCheckFunction) {
 	var testCases = {
-		"()(((())))": true,
-		"())))(": false,
-		")(": false
-	};
+        "()(((())))": true,
+        "())))(": false,
+        ")(": false,
+        "([{}])": true,
+        "([([[{(){}[()]}]])])": true,
+        "}{": false,
+        "[][][]{}(){[]}({})": true
+    };
+
+	// var testCases = {
+	// 	"()(((())))": true,
+	// 	"())))(": false,
+	// 	")(": false
+	// };
 	var allTestsPass = true;
 	for(parenString in testCases) {
 		var expected = testCases[parenString];
