e98eaa762b3416d0c450c02918e42cd227a0fc10
Jon Tejada <tejjon@gmail.com>
2015-12-17 09:25:24 -0800

    key adding function

---
+++ addkeys.js
@@ -0,0 +1,41 @@
+var obj1 = {
+	2:"ellie",
+	3:"liz",
+	"-1":"no",
+	"awesome":"cho",
+	"NaN":"something"
+};
+
+
+
+
+
+function addKeys(inputObj) {
+	var keyArr = Object.keys(inputObj);
+	var total = keyArr.reduce(function(a,b){
+		var nA = 0;
+		var nB = 0;
+		if (!isNaN(a)) {
+			nA = Number(a);
+		}
+		if (!isNaN(b)) {
+			nB = Number(b);
+		}
+		return nA + nB;
+	});
+	console.log(total);
+}
+addKeys(obj1);
+
+
+function addKeys2(inputObj) {
+	//second method here
+	
+}
+
+
+
+
+
+
+
