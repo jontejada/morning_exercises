7a09fd2a6c11e4b0d9bfb323696c4372c71d784b
Jon Tejada <tejjon@gmail.com>
2015-12-18 09:24:54 -0800

    sumargs with slice and reduce

--- sumArgs.js
+++ sumArgs.js
@@ -1,16 +1,18 @@
 function sumArguments(){
-	// // console.log(arguments);
-	// var argArr = arguments;
-	// console.log(argArr);
-	var total = 0;
-	for (var i = 0; i< arguments.length; i++) {
-		total +=arguments[i];
-	}
+	
+	//simple for loop:
+	// var total = 0;
+	// for (var i = 0; i< arguments.length; i++) {
+	// 	total +=arguments[i];
+	// }
+	
+	//slice and reduce:
+	var args = Array.prototype.slice.call(arguments);
+	var total = args.reduce(function(a,b){
+		return a + b;
+	});
+
 	console.log(total);
 }
-
-// var in1 = [1,2,3,4,5];
-// sumArguments([...in1]);
-
 
 sumArguments(1,2,3,4,-5);
