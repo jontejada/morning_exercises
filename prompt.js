2a6ea8b5a3ee54bb336ca3cb5483c1a55e9bc2ee
Jon Tejada <jon@jontejada.com>
2016-01-08 16:31:27 -0800

    today

---
+++ prompt.js
@@ -0,0 +1,39 @@
+
+// Your goal is to create a command line game, where users
+// guess a randomly generated number between 1-100 until 
+// they have guessed correctly. 
+
+var prompt = require('prompt');
+var randomNumber = Math.floor(Math.random() * 100) ; // fix this to be random
+console.log('random', randomNumber);
+var globalGuess = undefined;
+
+prompt.start();
+
+function ask() {
+	prompt.get(['guess'], function (err, result) {
+		globalGuess = result.guess;
+	});
+	// return globalGuess;
+}
+
+function game() {
+	ask();
+	if (globalGuess !== randomNumber) {
+		game();
+	} else {
+		console.log('correct');
+	}
+}
+
+game();
+
+// Create a game, where users can guess until they guess correctly.
+
+
+// while (globalGuess !== randomNumber) {
+// 	prompt.get(['guess'], function (err, result) {
+// 		globalGuess = result.guess;
+// 	});
+// }
+// console.log('correct');
