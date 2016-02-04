66d5f1974c703496f22c2587d6e0220c40133807
Jon Tejada <jon@jontejada.com>
2016-02-04 10:01:43 -0800

    regex update

--- regexmatching.js
+++ regexmatching.js
@@ -1,3 +1,6 @@
+// case-insensitive, ignore nums
+// return first matched char (ignoring nums), or return -1 if no matches
+
 // index:01234567
 var a = 'abcdA'; // 0
 var b = 'a1bcdec1'; // 3
@@ -5,9 +8,7 @@ var c = 'a1bcdeC1'; // 3
 var d = '1111aa1111'; // 4
 var e = '390483jz2098230g98230iz98320983205789809'; // 7
 
-// case-insensitive, ignore nums
-// return first matched char (ignoring nums), or return -1 if no matches
-
+// without regex:
 function getMatch(input) {
 	for (var i = 0; i < input.length; i++) {
 		var current = input[i].toLowerCase();
@@ -20,12 +21,22 @@ function getMatch(input) {
 	return -1;
 }
 
+console.log('getMatch function:');
 console.log(getMatch(a));
 console.log(getMatch(b));
 console.log(getMatch(c));
 console.log(getMatch(d));
 console.log(getMatch(e));
 
+// with regex:
+function getMatchRegex(input) {
+	return input.search(/([a-z]).*\1/i);
+}
 
-//regex work:
-//	var cleanInput = input.replace(/[0-9]/g,'');
+console.log('getMatchRegex function:');
+console.log(getMatchRegex(a));
+console.log(getMatchRegex(b));
+console.log(getMatchRegex(c));
+console.log(getMatchRegex(d));
+console.log(getMatchRegex(e));
+
