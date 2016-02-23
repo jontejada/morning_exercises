208832b39560eefbbee0f4c5c555a17712b94db3
Jon Tejada <jon@jontejada.com>
2016-02-23 09:28:11 -0800

    find outlier in array kata

---
+++ odd_even_array.js
@@ -0,0 +1,25 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The integers in the array are either entirely odd or entirely even except for a single integer N. Write a method that takes the array as an argument and returns N.
+// For example:
+var arr1 = [2, 4, 0, 100, 4, 11, 2602, 36];
+console.log(u(arr1),11);
+// Should return: 11
+
+var arr2 = [160, 3, 1719, 19, 11, 13, -21];
+console.log(u(arr2),160);
+// Should return: 160
+
+function u(arr) {
+	var odds = [];
+	var evens = [];
+	for (var i = 0; i<arr.length; i++) {
+		if (arr[i]%2 === 0) {
+			evens.push(arr[i]);
+		} else {
+			odds.push(arr[i]);
+		}
+	}
+	if (odds.length === 1) return odds[0];
+	if (evens.length === 1) return evens[0];
+
+}
+
