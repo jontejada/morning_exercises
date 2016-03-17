e8d33986aac0ee0401e74e26b20761071e9e7192
Jon Tejada <jon@jontejada.com>
2016-03-17 09:17:34 -0700

    notes added

---
+++ cracking/overview.md
@@ -0,0 +1,38 @@
+#data structures
+
+##linked lists
+
+##trees, tries, graphs
+
+##stacks, queues
+
+##heaps
+
+##vectors / arrayLists
+
+##hash tables
+
+#algorithms
+
+##breadth-first search
+
+##depth-first search
+
+##binary search
+
+##merge sort
+
+##quick sort
+
+#concepts
+
+##bit manipulation
+
+##memory
+stack vs heap
+
+##recursion
+
+##dynamic programmign
+
+##bigO time and space
--- cracking/sumLinkedLists.js
+++ cracking/sumLinkedLists.js
@@ -1,58 +1,70 @@
 function sum(n1, n2) {
-	var out = {},
-		current = out,
-		tens = 0,
-		firstVal = 0,
-		secondVal = 0,
-		sum = 0;
-	while(n1 || n2) {
-		firstVal = 0;
-		secondVal = 0;
-		if (n1) {
-			firstVal = n1.value;
-			n1 = n1.next;
-		}
-		if (n2) {
-			secondVal = n2.value;
-			n2 = n2.next;
-		}
-		sum = firstVal + secondVal + tens;
-		current.value = sum%10;
-		sum > 9 ? tens = 1 : tens = 0;
-		var nextNode = {};
-		current.next = nextNode;
-		current = nextNode;
-	}
-	return out;
+    var out = {},
+        current = out,
+        tens = 0,
+        firstVal = 0,
+        secondVal = 0,
+        sum = 0;
+    while(n1 || n2 || tens) {
+        firstVal = 0;
+        secondVal = 0;
+        if (n1) {
+            firstVal = n1.value;
+            n1 = n1.next;
+        }
+        if (n2) {
+            secondVal = n2.value;
+            n2 = n2.next;
+        }
+        sum = firstVal + secondVal + tens;
+        current.value = sum%10;
+        sum > 9 ? tens = 1 : tens = 0;
+        var nextNode = {};
+        current.next = nextNode;
+        current = nextNode;
+    }
+    return out;
 }
 
 var d = {
-	value: 3,
-	next: null
+    value: 3,
+    next: null
 };
 var c = {
-	value: 6,
-	next: d
+    value: 6,
+    next: d
 };
 var b = {
-	value: 1,
-	next: c
+    value: 1,
+    next: c
 };
 var a = {
-	value:7, 
-	next: b
+    value:7, 
+    next: b
 };
 var g = {
-	value: 2,
-	next: null
+    value: 2,
+    next: null
 };
 var f = {
-	value: 9,
-	next: g
+    value: 9,
+    next: g
 };
 var e = {
-	value: 5,
-	next: f
+    value: 5,
+    next: f
 };
 
-sum(a,e);
+console.log(sum(a,e));
+
+var h = {
+    value: 9,
+    next: null
+};
+
+var i = {
+    value: 8,
+    next:null
+};
+
+// console.log(sum(h,i));
